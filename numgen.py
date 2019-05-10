cf92147e32f7efe02c9c6fa0ba19c29b80bb06e0
borisdibon <48718209+borisdibon@users.noreply.github.com>
2019-05-10 18:55:27 +0200

    Update numgen.py

--- numgen.py
+++ numgen.py
@@ -1,32 +1,4 @@
 import random
 
-def permutation(lst):
-
-    if len(lst) == 0:
-        return []
-
-    if len(lst) == 1:
-        return [lst]
-
-
-    l = []
-
-    for i in range(len(lst)):
-        m = lst[i]
-        remLst = lst[:i] + lst[i+1:]
-        for p in permutation(remLst):
-            l.append([m] + p)
-    return l
-
-#data = []
-#n = 20
-#i = 0
-#while i < n:
-#   data.append(i)
-#    i += 1
-
-#for p in permutation(data):
-#   print ' '.join(map(str, p))
-
 list = random.sample(range(0, 5000), 500);
 print ' '.join(map(str, list))
